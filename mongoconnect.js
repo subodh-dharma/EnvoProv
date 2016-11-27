ce8805ff94c93a59bb6514d6f04ef92773d209be
ssdharma <ssdharma@ncsu.edu>
2016-11-27 16:16:06 -0500

    mongo connect

--- mongoconnect.js
+++ mongoconnect.js
@@ -2,7 +2,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
     if (!err) {
         console.log("We are connected");
     }
